0ae58b34fc05dc8994482d3e736550fdbf0b9f51
Richano <rishanogopal@gmail.com>
2023-12-18 03:36:42 +0100

    API fixes & add dotnet deploy action

--- thewhites-api/Program.cs
+++ thewhites-api/Program.cs
@@ -7,10 +7,17 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var root = Directory.GetCurrentDirectory();
+var dotenv = Path.Combine(root, ".env");
+DotEnv.Load(dotenv);
+
 // Add services to the container.
 
 builder.Services.AddDbContext<AspDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
+    options.UseSqlServer(
+        $"Data Source={Environment.GetEnvironmentVariable("MSSQL_CON_HOST")},{Environment.GetEnvironmentVariable("MSSQL_CON_PORT")};Database={Environment.GetEnvironmentVariable("MSSQL_CON_DB")};Integrated Security=false;User ID={Environment.GetEnvironmentVariable("MSSQL_CON_USER")};Password={Environment.GetEnvironmentVariable("MSSQL_CON_PASS")};"
+    )
+);
 
 builder.Services.AddScoped<IGebruikerRepository, GebruikerRepository>();
 
@@ -23,16 +30,6 @@ builder.Services.AddAuthentication().AddCookie().AddGoogle(GoogleDefaults.Authen
     options.ClientId = "712169306292-99g45f7oiuu0fe4b226j48cl02fo7qsj.apps.googleusercontent.com";
     options.ClientSecret = "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";
 });
-
-
-
-builder.Services
-    .AddHttpClient("CoopService", client =>
-    {
-        //client.BaseAddress = configuration.BaseAddress;
-    })
-    .AddHttpMessageHandler<MySpecialHeaderDelegatingHandler>();
-
 
 var app = builder.Build();
 
---
+++ thewhites-api/README.md
@@ -0,0 +1,8 @@
+## Info
+
+
+### Migrations
+
+Gebruiker het volgende om migrations te gebruiken:
+- `dotnet restore`
+- `dotnet tool install --global dotnet-ef --version 6.*`
