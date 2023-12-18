3dc6c09d6ee75ae8ac1a3352cec7977a7bf1b6a4
Richano <rishanogopal@gmail.com>
2023-12-18 04:40:44 +0100

    Fix workflow file

--- thewhites-api/Models/Beperking.cs
+++ thewhites-api/Models/Beperking.cs
@@ -8,6 +8,7 @@ namespace AspTest.Models
         public string Naam {get; set; }
         public string Omschrijving {get; set; }
 
+
         public ICollection<ErvaringsdeskundigeBeperking> ErvaringsdeskundigeBeperkingen {get; set;}
     }
 }
