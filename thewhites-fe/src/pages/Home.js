97ae993fd475a5e3712248ca8479be40b4cd7d57
Cazend <hiresh1010@gmail.com>
2023-12-21 03:13:02 +0100

    Add tekst & plaatje van home page

--- thewhites-fe/src/pages/Home.js
+++ thewhites-fe/src/pages/Home.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import handi from "../images/handi-op-tablet.png";
 
-import './Home.css';
+import "./Home.css"
 
 const Home = () => {
     return (
-        <h1>home</h1>
+        <Container>
+            <Row>
+                <Col lg="4" className="pt-5 p-2 text-start">
+                    <h4>Welkom op de website van Stichting Accesibility</h4>
+                </Col>
+            </Row>
+            <Row>
+                <Col lg="4" className="p-2 text-start">
+                    <h5>Waar we streven naar hoogste normen van gebruiksvriendelijkheid en toegankelijkheid. Voor iedereen, ongeacht hun achtergrond of mogelijkheden.</h5>
+                </Col>
+            </Row>
+            <Row>
+                <Col lg="6">
+                    <img src={handi} />
+                </Col>
+            </Row>
+        </Container>
     );
 };
 export default Home;
