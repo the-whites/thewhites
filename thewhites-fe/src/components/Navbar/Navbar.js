1742122931a8e579f8ab53a85c075fbb183ac01d
Cazend <hiresh1010@gmail.com>
2023-12-25 20:06:00 +0100

    Add folders

--- thewhites-fe/src/components/Navbar/Navbar.js
+++ thewhites-fe/src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import logo from "../images/sa-logo.png";
+import logo from "../assets/sa-logo.png";
 
 import "./Navbar.css";
 
@@ -12,8 +12,8 @@ const NavigationBar = () => {
 		<Navbar expand="lg" className="nav-bar">
 			<Container>
 				<Navbar.Brand as={Link} to="/"><img src={logo} width={375} height={100} /></Navbar.Brand>
-				<Navbar.Toggle aria-controls="dombo" />
-				<Navbar.Collapse id="dombo">
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
 						<Nav.Link as={Link} to="over-ons" className="nav-bar-item">Over ons</Nav.Link>
 						<Nav.Link as={Link} to="contact" className="nav-bar-item">Contact</Nav.Link>
