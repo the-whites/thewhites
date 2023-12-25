54ccbb2afba4b1a5713328c8c491502f180cd52d
Richano <rishanogopal@gmail.com>
2023-12-25 09:48:56 +0100

    Revert https change

--- thewhites-api/Program.cs
+++ thewhites-api/Program.cs
@@ -89,10 +89,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-else
-{
-    app.UseHttpsRedirection();
-}
+
+app.UseHttpsRedirection();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 app.UseAuthentication();
