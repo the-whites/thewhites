001285efd21a62f6204dc85fc2170448f9e92c3b
22013024-Romano <22013024@student.hhs.nl>
2023-12-29 16:01:58 +0100

    beheerdesportal test dashboard layout test

--- thewhites-fe/src/pages/BeheerderPortal/BeheerderPortal.js
+++ thewhites-fe/src/pages/BeheerderPortal/BeheerderPortal.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Container, Row, Col, Button, Navbar, Nav } from 'react-bootstrap';
+
+const Beheerdersportal = () => {
+    const [beheerdersnaam] = useState('Uw Naam');
+
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Beheerdersdashboard</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#gebruikers">Gebruikers</Nav.Link>
+                        <Nav.Link href="#rapporten">Rapporten</Nav.Link>
+                        <Nav.Link href="#instellingen">Instellingen</Nav.Link>
+                    </Nav>
+                    <Button variant="outline-info">Uitloggen</Button>
+                </Container>
+            </Navbar>
+            <Container className="mt-4">
+                <Row>
+                    <Col lg={12}>
+                        <h2>Welkom, {beheerdersnaam}</h2>
+                        <p>Beheer de inhoud van uw site en gebruikersaccounts snel en gemakkelijk.</p>
+                    </Col>
+                </Row>
+                {/* Extra beheerfunctionaliteiten kunnen hier worden toegevoegd */}
+            </Container>
+        </div>
+    );
+};
+
+export default Beheerdersportal;
+
+
+
+
+/*import React from "react";
 
 const BeheerderPortal = () => {
 	return (
 		<>
-			<p>BeheerderPortal Test</p>
+			<h4>BeheerderPortal Test</h4>
 		</>	
 	);
 };
 export default BeheerderPortal;
+*/
