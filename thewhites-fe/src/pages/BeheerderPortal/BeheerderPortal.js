0e0079ef882f526e0f9ba2ef1c2cf5b230f34de7
22013024-Romano <22013024@student.hhs.nl>
2023-12-29 22:16:51 +0100

    aanpassingen

--- thewhites-fe/src/pages/BeheerderPortal/BeheerderPortal.js
+++ thewhites-fe/src/pages/BeheerderPortal/BeheerderPortal.js
@@ -1,44 +1,47 @@
-import React, { useState } from 'react';
-import { Container, Row, Col, Button, Navbar, Nav } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import './BeheerderPortal.css'; 
+import React, { useState } from "react";
+import { Container, Row, Col, Button, Navbar, Nav } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import "./BeheerderPortal.css"; 
 
 //
 const Beheerdersportal = () => {
-    const [beheerdersnaam] = useState('Test Beheerder'); // moet de beheerdersnaam nog uit de database halen/ die inlogd
+	const [beheerdersnaam] = useState("Test Beheerder"); // moet de beheerdersnaam nog uit de database halen/ die inlogd
 	const navigate = useNavigate();
 
 	const handleLogout = () => {
-        // Voer hier uitloglogica uit indien nodig, zoals state bijwerken, cookies verwijderen, etc.
+		// Voer hier uitloglogica uit indien nodig, zoals state bijwerken, cookies verwijderen, etc.
         
-        // Redirect naar homepagina
-        navigate('/'); 
-    };
+		// Redirect naar homepagina
+		navigate("/"); 
+	};
 
-    return (
-        <div>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="home">Beheerdersdashboard</Navbar.Brand>
-                    <Nav className="me-auto">
+
+    //components van maken button en navbar
+    // en route toevoegen voor elke tabblad
+	return (
+		<div>
+			<Navbar bg="dark" variant="dark">
+				<Container>
+					<Navbar.Brand href="home">Beheerdersdashboard</Navbar.Brand>
+					<Nav className="me-auto">
 						<Nav.Link href="Ervaringsdeskundige">Ervaringsdeskundige</Nav.Link>
 						<Nav.Link href="Onderzoeken">Onderzoeken</Nav.Link>
 						<Nav.Link href="Bedrijven">Bedrijven</Nav.Link>
-                    </Nav>
-                    <Button variant="outline-info" className="btn-uitloggen" onClick={handleLogout} >Uitloggen</Button>
-                </Container>
-            </Navbar>
-            <Container className="mt-4">
-                <Row>
-                    <Col lg={12}>
-                        <h2>Welkom, {beheerdersnaam}</h2>
-                        <p>Beheer de inhoud van uw site en gebruikersaccounts snel en gemakkelijk.</p>
-                    </Col>
-                </Row>
-                {/* Extra beheerfunctionaliteiten kunnen hier worden toegevoegd */}
-            </Container>
-        </div>
-    );
+					</Nav>
+					<Button variant="outline-info" className="btn-uitloggen" onClick={handleLogout} >Uitloggen</Button>
+				</Container>
+			</Navbar>
+			<Container className="mt-4">
+				<Row>
+					<Col lg={12}>
+						<h2>Welkom, {beheerdersnaam}</h2>
+						<p>Beheer de inhoud van uw site en gebruikersaccounts snel en gemakkelijk.</p>
+					</Col>
+				</Row>
+				{/* Extra beheerfunctionaliteiten kunnen hier worden toegevoegd */}
+			</Container>
+		</div>
+	);
 };
 
 export default Beheerdersportal;
