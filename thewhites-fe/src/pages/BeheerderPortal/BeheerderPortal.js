a364c09774bbdc9f0194bb1901510df5a062c7b6
22013024-Romano <22013024@student.hhs.nl>
2023-12-30 15:31:30 +0100

    pad wijzingen

--- thewhites-fe/src/pages/BeheerderPortal/BeheerderPortal.js
+++ thewhites-fe/src/pages/BeheerderPortal/BeheerderPortal.js
@@ -1,36 +1,28 @@
 import React, { useState } from "react";
-import { Container, Row, Col, Button, Navbar, Nav } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import NavbarPortal from "components/NavbarPortal";
+import ButtonPortal from "components/ButtonPortal";
+import { Link } from "react-router-dom";
 import "./BeheerderPortal.css"; 
+import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
 
-//
 const Beheerdersportal = () => {
 	const [beheerdersnaam] = useState("Test Beheerder"); // moet de beheerdersnaam nog uit de database halen/ die inlogd
-	const navigate = useNavigate();
-
-	const handleLogout = () => {
-		// Voer hier uitloglogica uit indien nodig, zoals state bijwerken, cookies verwijderen, etc.
-        
-		// Redirect naar homepagina
-		navigate("/"); 
-	};
-
-
-    //components van maken button en navbar
-    // en route toevoegen voor elke tabblad
+	
+	//components van maken button en navbar
+	// en route toevoegen voor elke tabblad
 	return (
 		<div>
-			<Navbar bg="dark" variant="dark">
+			<NavbarPortal bg="dark" variant="dark">
 				<Container>
-					<Navbar.Brand href="home">Beheerdersdashboard</Navbar.Brand>
+					<Navbar.Brand as={Link} to="/">Beheerdersdashboard</Navbar.Brand>
 					<Nav className="me-auto">
-						<Nav.Link href="Ervaringsdeskundige">Ervaringsdeskundige</Nav.Link>
-						<Nav.Link href="Onderzoeken">Onderzoeken</Nav.Link>
-						<Nav.Link href="Bedrijven">Bedrijven</Nav.Link>
+						<Nav.Link as={Link} to="/ervaringsdeskundige">Ervaringsdeskundige</Nav.Link>
+						<Nav.Link as={Link} to="/onderzoeken">Onderzoeken</Nav.Link>
+						<Nav.Link as={Link} to="/bedrijven">Bedrijven</Nav.Link>
 					</Nav>
-					<Button variant="outline-info" className="btn-uitloggen" onClick={handleLogout} >Uitloggen</Button>
+					<ButtonPortal/>
 				</Container>
-			</Navbar>
+			</NavbarPortal>
 			<Container className="mt-4">
 				<Row>
 					<Col lg={12}>
