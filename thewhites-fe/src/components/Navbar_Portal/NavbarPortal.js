8a4d9f45e896516a523e052ab94fef25bf58b1b6
22013024-Romano <22013024@student.hhs.nl>
2023-12-30 19:12:19 +0100

    components gefixt

--- thewhites-fe/src/components/Navbar_Portal/NavbarPortal.js
+++ thewhites-fe/src/components/Navbar_Portal/NavbarPortal.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Navbar, Container } from "react-bootstrap";
 import "./NavbarPortal.css";
-
-const NavbarPortal = () => {
+const NavbarPortal = ({ children }) => {
 	return (
 		<Navbar bg="dark" variant="dark">
 			<Container>
-				<Navbar.Brand as={Link} to=" "></Navbar.Brand>
-				<Nav className=" ">
-					<Nav.Link as={Link} to=" "></Nav.Link>
-					<Nav.Link as={Link} to=" "></Nav.Link>
-					<Nav.Link as={Link} to=" "></Nav.Link>
-				</Nav>
+				{children}
 			</Container>
 		</Navbar>
 	);
--- thewhites-fe/src/pages/BeheerderPortal/BeheerderPortal.js
+++ thewhites-fe/src/pages/BeheerderPortal/BeheerderPortal.js
@@ -13,15 +13,13 @@ const Beheerdersportal = () => {
 	return (
 		<div>
 			<NavbarPortal bg="dark" variant="dark">
-				<Container>
-					<Navbar.Brand as={Link} to="/">Beheerdersdashboard</Navbar.Brand>
-					<Nav className="me-auto">
-						<Nav.Link as={Link} to="/ervaringsdeskundige">Ervaringsdeskundige</Nav.Link>
-						<Nav.Link as={Link} to="/onderzoeken">Onderzoeken</Nav.Link>
-						<Nav.Link as={Link} to="/bedrijven">Bedrijven</Nav.Link>
-					</Nav>
-					<ButtonPortal/>
-				</Container>
+				<Navbar.Brand as={Link} to="/">Beheerdersdashboard</Navbar.Brand>
+				<Nav className="me-auto">
+					<Nav.Link as={Link} to="/ervaringsdeskundige">Ervaringsdeskundige</Nav.Link>
+					<Nav.Link as={Link} to="/onderzoeken">Onderzoeken</Nav.Link>
+					<Nav.Link as={Link} to="/bedrijven">Bedrijven</Nav.Link>
+				</Nav>
+				<ButtonPortal/>
 			</NavbarPortal>
 			<Container className="mt-4">
 				<Row>
