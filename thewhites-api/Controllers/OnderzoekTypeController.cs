4af6a10949c8c1b0191e298fe92049e866b1b83a
Cazend <hiresh1010@gmail.com>
2024-01-03 19:10:34 +0100

    Add beperking & onderzoektype controllers

---
+++ thewhites-api/Controllers/OnderzoekTypeController.cs
@@ -0,0 +1,28 @@
+using AspTest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OnderzoekTypeController : Controller
+    {
+        private readonly IOnderzoekTypeRepository onderzoekTypeRepository;
+
+        public OnderzoekTypeController(IOnderzoekTypeRepository onderzoekTypeRepository)
+        {
+            this.onderzoekTypeRepository = onderzoekTypeRepository;
+        }
+
+        [HttpGet("onderzoek-types")]
+        public IActionResult GetOnderzoekTypes()
+        {
+            ICollection<OnderzoekType> onderzoekTypeLijst = onderzoekTypeRepository.GetOnderzoekTypes(); 
+            
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(onderzoekTypeLijst);
+        }
+    }
+}
