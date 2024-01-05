1666b8eb67dca9257e5b4de20733d1703e4b9ad8
Cazend <hiresh1010@gmail.com>
2024-01-05 23:33:41 +0100

    wat comments achter laten want waarom niet

--- thewhites-fe/src/pages/Layout.js
+++ thewhites-fe/src/pages/Layout.js
@@ -1,3 +1,4 @@
+// I.P.V. layout kan toastcontainer miss in app.js? aangezien het voor niks anders wordt gebruikt nu
 import React from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
