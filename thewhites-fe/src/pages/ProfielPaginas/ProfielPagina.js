6cc8bc2b5a352d3a7fbf443beccdc9aa082ac562
22013024-Romano <22013024@student.hhs.nl>
2024-01-11 18:46:59 +0100

    pagina aangepast

--- thewhites-fe/src/pages/ProfielPaginas/ProfielPagina.js
+++ thewhites-fe/src/pages/ProfielPaginas/ProfielPagina.js
@@ -1,47 +1,75 @@
 import React, { useState, useEffect } from "react";
-import { Button, Col, Container, Form, Row, Table } from "react-bootstrap";
+import { Button, Container, Form, Row } from "react-bootstrap";
 import InputBar from "../../components/Inputbar/InputBar";
 import MultiSelectionBar from "../../components/MultiSelectionBar/MultiSelectionBar";
 
-const BewerkProfielForm = ({ setProfielData = (items) => {}, handleSubmitForm = ( ) => {}, beperkingItems, onderzoekTypeItems }) => {
-	// Initialize the profielData state with empty values within the component
+const BewerkProfielForm = ({ setProfielData = (items) => {}, handleSubmitForm = () => {}, beperkingItems, onderzoekTypeItems }) => {
 	const [profielData, setInternalProfielData] = useState({
 		voornaam: "",
 		achternaam: "",
 		postcode: "",
 		emailadres: "",
 		telefoonnummer: "",
-		beperkingTypes: [""],
+		beperkingTypes: [],
 	});
 
-  
-	// This function is used to update the internal state and the parent state if necessary
-	const updateProfielData = (updates) => {
-		const newProfielData = { ...profielData, ...updates };
+	useEffect(() => {
+		const savedData = localStorage.getItem("profielData");
+		if (savedData) {
+			setInternalProfielData(JSON.parse(savedData));
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem("profielData", JSON.stringify(profielData));
+	}, [profielData]);
+
+	// This function is used to update both the internal state and the parent state, if necessary
+	const updateProfielData = (name, value) => {
+		const newProfielData = { ...profielData, [name]: value };
 		setInternalProfielData(newProfielData);
 		setProfielData(newProfielData); // Update the parent component's state, if needed
 	};
-  
+
 	return (
 		<Form validated={true} onSubmit={(event) => handleSubmitForm(event, profielData)}>
 			<Container>
 				<Row className="justify-content-md-center">
-					<InputBar label="Voornaam" required value={profielData.voornaam} handleChange={(value) => updateProfielData("voornaam", value)} />
-					<InputBar label="Achternaam" required value={profielData.achternaam} handleChange={(value) => updateProfielData("achternaam", value)} />
-					<InputBar label="Postcode" required value={profielData.postcode} handleChange={(value) => updateProfielData("postcode", value)} />
-					<InputBar label="E-mailadres" required value={profielData.emailadres} handleChange={(value) => updateProfielData("emailadres", value)} />
-
 					<InputBar 
+						label="Voornaam" 
 						required 
-						value={profielData?.telefoonnummer || ""} 
-						handleChange={(value) => setProfielData({...profielData, telefoonnummer: value})} 
+						value={profielData.voornaam} 
+						handleChange={(value) => updateProfielData("voornaam", value)} 
+					/>
+					<InputBar 
+						label="Achternaam" 
+						required 
+						value={profielData.achternaam} 
+						handleChange={(value) => updateProfielData("achternaam", value)} 
+					/>
+					<InputBar 
+						label="Postcode" 
+						required 
+						value={profielData.postcode} 
+						handleChange={(value) => updateProfielData("postcode", value)} 
+					/>
+					<InputBar 
+						label="E-mailadres" 
+						required 
+						value={profielData.emailadres} 
+						handleChange={(value) => updateProfielData("emailadres", value)} 
+					/>
+					<InputBar 
+						label="Telefoonnummer" 
+						required 
+						value={profielData.telefoonnummer} 
+						handleChange={(value) => updateProfielData("telefoonnummer", value)} 
 						min={8}
-						label="Telefoonnummer"
 					/>
 					<MultiSelectionBar 
 						label="Type beperkingen" 
 						items={beperkingItems}
-						handleSelection={(selectedItems) => setProfielData({...profielData, beperkingTypes: selectedItems})}
+						handleSelection={(selectedItems) => updateProfielData("beperkingTypes", selectedItems)}
 						initialSelectedItems={profielData.beperkingTypes} 
 						getKey={(option) => option.id} 
 						getValue={(option) => option.naam}
@@ -53,5 +81,5 @@ const BewerkProfielForm = ({ setProfielData = (items) => {}, handleSubmitForm = 
 		</Form>
 	);
 };
+
 export default BewerkProfielForm;
-  
