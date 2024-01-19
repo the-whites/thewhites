6031234989ad9de10f09167ee46633107b17a8a1
Suraj <22040358@student.hhs.nl>
2024-01-19 21:34:53 +0100

    over ons page en contact page enzo

--- thewhites-fe/src/pages/OverOns/OverOns.js
+++ thewhites-fe/src/pages/OverOns/OverOns.js
@@ -1,11 +1,53 @@
 import React from "react";
+import "./OverOns.css";
 
 const OverOns = () => {
 	return (
-		<>
-			<h1>Over Ons</h1>
-			<p>Dit is leeg.</p>
-		</>	
+		<div className="over-ons">
+			<h1 className="header">Over Ons</h1>
+			<p>Stichting Accessibility zet zich in voor een digitaal, fysiek en sociaal toegankelijke samenleving.</p>
+			<p>Een samenleving waarin iedereen zelfstandig kan meedoen en zich optimaal kan ontplooien, ook als je een</p> 
+			<p>beperking hebt. Accessibility ondersteunt (maatschappelijke) organisaties bij het realiseren van</p> 
+			<p>toegankelijkheid. Zo dragen wij bij aan een inclusieve samenleving met voorzieningen die toegankelijk en</p> 
+			<p>bruikbaar zijn voor iedereen. </p>
+			<h1 className="onze-missie">Onze Missie</h1>
+			<p>Bij Accessibility werken we aan een inclusieve samenleving waarin iedereen kan meedoen</p>
+			<p>en tot zijn recht komt. Steeds meer organisaties sluiten zich aan bij onze ambities. Al zoeken</p>
+			<p>ze nog naar hoe ze dit voor elkaar kunnen krijgen.</p>
+			<br></br>
+			<p>Samen met onze klanten en partners bouwen we iedere dag aan een toegankelijker</p>
+			<p>Nederland. We zetten onze kennis en expertise in om fysieke, sociale én digitale</p>
+			<p>omgevingen toegankelijk te maken; in het bijzonder voor mensen met een (visuele)</p>
+			<p>beperking.</p>
+			<br></br>
+			<p>Onze experts denken graag met hen mee en bieden begeleiding op maat. Dit doen zij in</p>
+			<p>nauwe samenwerking met ervaringsdeskundigen. Met onze trainingen, producten en</p>
+			<p>diensten kan iedere organisatie morgen nog aan de slag met toegankelijkheid. Zo vertalen</p>
+			<p>we onze kennis naar joúw praktijk.</p>
+			<br></br>
+			<p>Wij denken in mogelijkheden – iedere dag opnieuw – binnen jouw doelen en budget. Zodat</p>
+			<p>jouw organisatie doet waar jullie voor staan; voor collega’s, klanten en/of burgers.</p>
+			<p>Jouw investering in toegankelijkheid is waardevol voor jouw bedrijf én voor de samenleving.</p>
+			<br></br>
+			<p>Via Stichting Accessibility vloeit jouw investering terug naar projecten die bijdragen aan een</p>
+			<p>toegankelijke en inclusieve samenleving. Sluit ook bij ons aan!</p>
+			<h1 className="achtergrond">Achtergrond</h1>
+			<p>Begin deze eeuw digitaliseerde de samenleving in een rap tempo. Maar waren al die</p>
+			<p>websites en digitale hulpmiddelen wel door iedereen te gebruiken? Toegankelijkheid stond</p>
+			<p>nog in de kinderschoenen. Als eerste organisatie in Nederland begon Stichting Accessibility</p>
+			<p>in 2001 met het toegankelijk maken van ICT voor mensen met een visuele beperking.</p>
+			<br></br>
+			<p>Later verbreedden wij onze aandacht naar toegankelijkheid voor álle mensen met een</p>
+			<p>(tijdelijke) beperking. Hierdoor zijn we sinds 2020 ook actief in de fysieke en sociale</p>
+			<p>omgeving. Met onze experts werken we elke dag aan het verankeren van toegankelijkheid in</p>
+			<p>organisaties in Nederland; van overheidsinstellingen tot bedrijven. Gelukkig kunnen we</p>
+			<p>hierin met steeds meer partnerorganisaties optrekken.</p>
+			<br></br>
+			<p>Onze beweging blijft niet onopgemerkt. De regelgeving beweegt mee: mede dankzij onze</p>
+			<p>inspanningen is de internationale richtlijn op het gebied van digitale websites en apps, de</p>
+			<p>WCAG, gerealiseerd. Daarnaast denken we mee met diverse richtlijnen op inclusief</p>
+			<p>design en fysieke toegankelijkheid.</p>
+		</div>	
 	);
 };
 export default OverOns;
