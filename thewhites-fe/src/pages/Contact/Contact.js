ef717eed5211f1435f2461d396e1ee6d6ce275e5
Suraj <22040358@student.hhs.nl>
2024-01-20 17:29:43 +0100

    privacy policy page

--- thewhites-fe/src/pages/Contact/Contact.js
+++ thewhites-fe/src/pages/Contact/Contact.js
@@ -7,14 +7,14 @@ const Contact = () => {
 			<h1 className="header">Contact</h1>
 			<p>Stichting Accessibility is gevestigd in het bedrijfsverzamelgebouw de Krammstate op</p>
 			<p>een paar minuten lopen van Station Utrecht Overvecht.</p>
-			<br></br>
+			<br />
 			<h3 className="adres">Bezoek- en postadres:</h3>
 			<p>Christiaan Krammlaan 2</p>
 			<p>3571 AX Utrecht</p>
-			<br></br>
+			<br />
 			<h3 className="contact-opnemen">Contact opnemen:</h3>
 			<p>Tel. +31 30 239 82 70</p>
-			<p>E-mail: <a href="mailto:info@accessibility.nl">info@accessibilty.nl</a></p>
+			<p>E-mail: <a id="mail"href="mailto:info@accessibility.nl">info@accessibilty.nl</a></p>
 		</div>
 	);
 };
---
+++ thewhites-fe/src/pages/PrivacyPolicy/PrivacyPolicy.js
@@ -0,0 +1,29 @@
+/* eslint-disable indent */
+import React from "react";
+import "./PrivacyPolicy.css";
+
+const PrivacyPolicy = () => {
+	return (
+		<div className="privacy-policy">
+			<h1 className="header">Privacy Policy</h1>
+			<p>Bij Stichting Accessibility hechten wij veel waarde aan privacy en gegevensbescherming. Dit doen we</p>
+            <p>door het nemen van technische en organisatorische maatregelen. Hierdoor zorgen we dat uw</p>
+            <p>gegevens niet in de verkeerde handen vallen of als verkeerde doeleinden gebruikt worden.</p>
+            <br />
+            <p>De verbinding wordt ten allen tijde beveiligd op technische wijze. Dit doen we door de verbinding eerst vertrouwelijk</p>
+            <p>te maken door een certificaat op te geven die u verbindt met de correcte website. Verder worden al uw</p>
+            <p>gegevens tijdens een verzending versleuteld, zodat ze niet te lezen zijn.</p>
+            <br />
+            <p>Naast onze technische maatregelen zijn er ook organisatorische maatregelen genomen. Zo geven we</p>
+            <p>de juiste personen alleen toegang tot de benodigde hulpmiddelen op de website.</p>
+            <br />
+            <p>Stichting Accessibility zelf heeft als enige het recht om persoonsgegevens te beheren, voor als</p>
+            <p>doeleinden het corrigeren van fouten in profielen van ervaringsdeskundigen.</p>
+            <h1 className="contact-privacy">Contact</h1>
+            <p>Indien u nog vragen mocht hebben over de Privacy Policy van Accessibility, dan kunt u <a id = "oke" href="/contact">contact</a> met</p>
+            <p>ons opnemen. Wij helpen u graag verder als u informatie nodig heeft over uw gegevens</p>
+            <p>of als u deze wilt wijzigen.</p>
+        </div>	
+	);
+};
+export default PrivacyPolicy;
