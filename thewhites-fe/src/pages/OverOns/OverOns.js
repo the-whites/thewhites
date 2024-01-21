91249118300e4743795f9c40ebc910d851068271
Cazend <hiresh1010@gmail.com>
2024-01-21 19:20:55 +0100

    styling

--- thewhites-fe/src/pages/OverOns/OverOns.js
+++ thewhites-fe/src/pages/OverOns/OverOns.js
@@ -1,44 +1,48 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import "./OverOns.css";
 import { Container } from "react-bootstrap";
 
 const OverOns = () => {
 	return (
 		<Container className="container-over-ons">
-			<h1>Over Ons</h1>
-			<p>
-        Stichting Accessibility zet zich in voor een digitaal, fysiek en sociaal toegankelijke samenleving.
-			</p>
-			<p>
-        Een samenleving waarin iedereen zelfstandig kan meedoen en zich optimaal kan ontplooien, ook als je een beperking hebt. Accessibility ondersteunt (maatschappelijke) organisaties bij het realiseren van toegankelijkheid. Zo dragen wij bij aan een inclusieve samenleving met voorzieningen die toegankelijk en bruikbaar zijn voor iedereen.
-			</p>
-			<h1 className="onze-missie">Onze Missie</h1>
-			<p>
-        Bij Accessibility werken we aan een inclusieve samenleving waarin iedereen kan meedoen en tot zijn recht komt. Steeds meer organisaties sluiten zich aan bij onze ambities. Al zoeken ze nog naar hoe ze dit voor elkaar kunnen krijgen.
-			</p>
-			<p>
-        Samen met onze klanten en partners bouwen we iedere dag aan een toegankelijker Nederland. We zetten onze kennis en expertise in om fysieke, sociale én digitale omgevingen toegankelijk te maken; in het bijzonder voor mensen met een (visuele) beperking.
-			</p>
-			<p>
-        Onze experts denken graag met hen mee en bieden begeleiding op maat. Dit doen zij in nauwe samenwerking met ervaringsdeskundigen. Met onze trainingen, producten en diensten kan iedere organisatie morgen nog aan de slag met toegankelijkheid. Zo vertalen we onze kennis naar jouw praktijk.
-			</p>
-			<p>
-        Wij denken in mogelijkheden – iedere dag opnieuw – binnen jouw doelen en budget. Zodat jouw organisatie doet waar jullie voor staan; voor collega’s, klanten en/of burgers. Jouw investering in toegankelijkheid is waardevol voor jouw bedrijf én voor de samenleving.
-			</p>
-			<p>
-        Via Stichting Accessibility vloeit jouw investering terug naar projecten die bijdragen aan een toegankelijke en inclusieve samenleving. Sluit ook bij ons aan!
-			</p>
-			<h1 className="achtergrond">Achtergrond</h1>
-			<p>
-        Begin deze eeuw digitaliseerde de samenleving in een rap tempo. Maar waren al die websites en digitale hulpmiddelen wel door iedereen te gebruiken? Toegankelijkheid stond nog in de kinderschoenen. Als eerste organisatie in Nederland begon Stichting Accessibility in 2001 met het toegankelijk maken van ICT voor mensen met een visuele beperking.
-			</p>
-			<p>
-        Later verbreedden wij onze aandacht naar toegankelijkheid voor álle mensen met een (tijdelijke) beperking. Hierdoor zijn we sinds 2020 ook actief in de fysieke en sociale omgeving. Met onze experts werken we elke dag aan het verankeren van toegankelijkheid in organisaties in Nederland; van overheidsinstellingen tot bedrijven. Gelukkig kunnen we hierin met steeds meer partnerorganisaties optrekken.
-			</p>
-			<p>
-        Onze beweging blijft niet onopgemerkt. De regelgeving beweegt mee: mede dankzij onze inspanningen is de internationale richtlijn op het gebied van digitale websites en apps, de WCAG, gerealiseerd. Daarnaast denken we mee met diverse richtlijnen op inclusief design en fysieke toegankelijkheid.
-			</p>
+			<Row className="mt-4 justify-content-center">
+				<Col md={9}>
+					<h1 className="over-ons-header">Over Ons</h1>
+					<p>
+						Stichting Accessibility zet zich in voor een digitaal, fysiek en sociaal toegankelijke samenleving.
+					</p>
+					<p>
+						Een samenleving waarin iedereen zelfstandig kan meedoen en zich optimaal kan ontplooien, ook als je een beperking hebt. Accessibility ondersteunt (maatschappelijke) organisaties bij het realiseren van toegankelijkheid. Zo dragen wij bij aan een inclusieve samenleving met voorzieningen die toegankelijk en bruikbaar zijn voor iedereen.
+					</p>
+					<h1 className="over-ons-header">Onze Missie</h1>
+					<p>
+						Bij Accessibility werken we aan een inclusieve samenleving waarin iedereen kan meedoen en tot zijn recht komt. Steeds meer organisaties sluiten zich aan bij onze ambities. Al zoeken ze nog naar hoe ze dit voor elkaar kunnen krijgen.
+					</p>
+					<p>
+						Samen met onze klanten en partners bouwen we iedere dag aan een toegankelijker Nederland. We zetten onze kennis en expertise in om fysieke, sociale én digitale omgevingen toegankelijk te maken; in het bijzonder voor mensen met een (visuele) beperking.
+					</p>
+					<p>
+						Onze experts denken graag met hen mee en bieden begeleiding op maat. Dit doen zij in nauwe samenwerking met ervaringsdeskundigen. Met onze trainingen, producten en diensten kan iedere organisatie morgen nog aan de slag met toegankelijkheid. Zo vertalen we onze kennis naar jouw praktijk.
+					</p>
+					<p>
+						Wij denken in mogelijkheden – iedere dag opnieuw – binnen jouw doelen en budget. Zodat jouw organisatie doet waar jullie voor staan; voor collega’s, klanten en/of burgers. Jouw investering in toegankelijkheid is waardevol voor jouw bedrijf én voor de samenleving.
+					</p>
+					<p>
+						Via Stichting Accessibility vloeit jouw investering terug naar projecten die bijdragen aan een toegankelijke en inclusieve samenleving. Sluit ook bij ons aan!
+					</p>
+					<h1 className="over-ons-header">Achtergrond</h1>
+					<p>
+						Begin deze eeuw digitaliseerde de samenleving in een rap tempo. Maar waren al die websites en digitale hulpmiddelen wel door iedereen te gebruiken? Toegankelijkheid stond nog in de kinderschoenen. Als eerste organisatie in Nederland begon Stichting Accessibility in 2001 met het toegankelijk maken van ICT voor mensen met een visuele beperking.
+					</p>
+					<p>
+						Later verbreedden wij onze aandacht naar toegankelijkheid voor álle mensen met een (tijdelijke) beperking. Hierdoor zijn we sinds 2020 ook actief in de fysieke en sociale omgeving. Met onze experts werken we elke dag aan het verankeren van toegankelijkheid in organisaties in Nederland; van overheidsinstellingen tot bedrijven. Gelukkig kunnen we hierin met steeds meer partnerorganisaties optrekken.
+					</p>
+					<p>
+						Onze beweging blijft niet onopgemerkt. De regelgeving beweegt mee: mede dankzij onze inspanningen is de internationale richtlijn op het gebied van digitale websites en apps, de WCAG, gerealiseerd. Daarnaast denken we mee met diverse richtlijnen op inclusief design en fysieke toegankelijkheid.
+					</p>
+				</Col>
+			</Row>
 		</Container>
 	);
 };
